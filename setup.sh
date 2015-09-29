feda07a91534eb1fc975856ad252e515ed96f78c
qpig <564388878@qq.com>
2015-09-29 15:29:09 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -2,13 +2,14 @@
 
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if ! [[ -e $HOME/.spacemacs ]]; then
+if ! [[ -L $HOME/.spacemacs ]] && [[ -f $HOME/.spacemacs ]]; then
 	echo "Link .spacemacs"
 	rm $HOME/.spacemacs
-	ln -s $THIS_DIR/spacemacs $HOME/.spacemacs	
+	ln -s $THIS_DIR/.spacemacs $HOME/.spacemacs	
 fi
 
 if ! [[ -L $HOME/.emacs.d/private ]] && [[ -d $HOME/.emacs.d/private ]]; then
+	echo "Link private"
 	mv $HOME/.emacs.d/private $HOME/.emacs.d/private.bak
 	ln -s $THIS_DIR $HOME/.emacs.d/private
 fi
