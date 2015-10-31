07837614baad67a9e28559e27f0fbd7cc80c048a
qpig <564388878@qq.com>
2015-10-31 21:12:07 +0800

    clone spacemacs

--- setup.sh
+++ setup.sh
@@ -2,16 +2,18 @@
 
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if ! [[ -L $HOME/.spacemacs ]] ; then
-	if [[ -e $HOME/.spacemacs ]]; then
-		rm $HOME/.spacemacs
-	fi
-	echo "Link .spacemacs"
-	ln -s $THIS_DIR/.spacemacs $HOME/.spacemacs	
+if [[ -e $HOME/.spacemacs ]]; then
+    rm $HOME/.spacemacs
 fi
+echo "Link .spacemacs"
+ln -s $THIS_DIR/.spacemacs $HOME/.spacemacs	
 
-if ! [[ -L $HOME/.emacs.d/private ]] && [[ -d $HOME/.emacs.d/private ]]; then
-	echo "Link private"
-	mv $HOME/.emacs.d/private $HOME/.emacs.d/private.bak
-	ln -s $THIS_DIR $HOME/.emacs.d/private
+echo "replace ~/.emacs.d"
+rm ~/.emacs.d -rf
+cp ./spacemacs ~/.emacs.d -r
+
+if [[ -e $HOME/.emacs.d/private ]] ;then
+    rm $HOME/.emacs.d/private -rf
+echo "Link private"
+ln -s $THIS_DIR $HOME/.emacs.d/private
 fi
