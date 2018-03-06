744910f29727992cd1d8b827cfc781fe000f68e5
qpig <564388878@qq.com>
2018-03-05 21:32:26 -0800

    config linux

--- layers/linux/layers.el
+++ layers/linux/layers.el
@@ -6,21 +6,26 @@
                                              c-c++-default-mode-for-headers 'c++-mode)
                                       (auto-completion :variables
                                                        auto-completion-return-key-behavior 'complete
-                                                       auto-completion-tab-key-behavior nil
+                                                       auto-completion-tab-key-behavior 'cycle
                                                        auto-completion-complete-with-key-sequence nil
                                                        auto-completion-complete-with-key-sequence-delay 0.1
+                                                       auto-completion-idle-delay 0.2
                                                        auto-completion-enable-help-tooltip t
                                                        auto-completion-private-snippets-directory nil)
-                                      syntax-checking
-                                      ;; spell-checking
+                                      (syntax-checking :variables syntax-checking-enable-by-default nil)
                                       org
 
-                                      version-control
+                                      (version-control :variables version-control-diff-side 'left)
 
-                                      ;; something I know
-                                      (ranger :variables ranger-cleanup-eagerly t)
+                                      (ranger :variables
+                                              ranger-cleanup-eagerly t
+                                              ranger-show-preview t
+                                              ranger-cleanup-on-disable t
+                                              ranger-show-hidden t)
                                       cscope
                                       (git :variables git-magit-status-fullscreen t)
                                       smex
-                                      (ibuffer :variables ibuffer-group-buffers-by 'projects)
+
+                                      (perforce)
+                                      (command-log)
                                       ))
